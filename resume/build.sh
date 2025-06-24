cb96d43fa0298a60340a89c64a92f41e2c48516b
Kseniia <97663572+Kseniiashk@users.noreply.github.com>
2025-06-25 08:06:40 +0900

    Update build.sh

--- resume/build.sh
+++ resume/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-set -e
+if [ ! -d /usr/share/texmf/tex/latex/altacv ]; then
+    mkdir -p /usr/share/texmf/tex/latex/altacv
+    cp altacv.cls /usr/share/texmf/tex/latex/altacv/
+    texhash
+fi
+
 pdflatex -interaction=nonstopmode resume.tex
-pdflatex -interaction=nonstopmode resume.tex
-echo "resume.pdf готово"
+pdflatex -interaction=nonstopmode resume.tex  
+rm -f *.aux *.log *.out *.bcf *.run.xml
