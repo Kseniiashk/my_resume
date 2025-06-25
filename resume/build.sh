f47207cfc1b78fad8d40afe9f1db53974414f169
Kseniia <97663572+Kseniiashk@users.noreply.github.com>
2025-06-25 21:46:50 +0900

    Update build.sh

--- resume/build.sh
+++ resume/build.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-sed -i 's/\\geometry{l/\\geometry{/' resume.tex
+rm -f altacv.cls
+
+wget https://raw.githubusercontent.com/liantze/AltaCV/main/altacv.cls
+
+mkdir -p /usr/share/texmf/tex/latex/altacv
+cp altacv.cls /usr/share/texmf/tex/latex/altacv/
+texhash
 
 fc-cache -fv
 
 lualatex -interaction=nonstopmode resume.tex
+lualatex -interaction=nonstopmode resume.tex
+
+if [ -f resume.pdf ]; then
+    cp resume.pdf /app/output/
+    echo "PDF успешно сгенерирован!"
+else
+    echo "ОШИБКА: PDF не сгенерирован"
+    ls -la
+    exit 1
+fi
