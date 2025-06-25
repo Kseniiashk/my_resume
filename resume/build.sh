112c8b7a00ca0716f37eb132266061233c43db79
Kseniia <97663572+Kseniiashk@users.noreply.github.com>
2025-06-25 17:58:02 +0900

    Update build.sh

--- resume/build.sh
+++ resume/build.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 
-# Установка локальных пакетов LaTeX
-if [ ! -d /usr/share/texmf/tex/latex/altacv ]; then
-    mkdir -p /usr/share/texmf/tex/latex/altacv
-    cp altacv.cls /usr/share/texmf/tex/latex/altacv/
-    texhash
-fi
+sed -i 's/\\geometry{l/\\geometry{/' resume.tex
 
-echo "### Доступные шрифты ###"
-fc-list
-echo "########################"
+fc-cache -fv
 
-sed -i 's/\\setmainfont{.*}/\\setmainfont{DejaVu Sans}/' resume.tex
-sed -i 's/\\geometry{l/\\geometry{/' resume.tex  
 lualatex -interaction=nonstopmode resume.tex
-lualatex -interaction=nonstopmode resume.tex 
-rm -f *.aux *.log *.out *.bcf *.run.xml
-
-if [ -f resume.pdf ]; then
-    cp resume.pdf /app/output/
-    echo "PDF успешно сгенерирован!"
-else
-    echo "ОШИБКА: PDF не сгенерирован"
-    ls -la
-    exit 1
-fi
