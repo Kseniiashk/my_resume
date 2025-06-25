9a5296eec0d4878a614d3c111ee76ddbd14bf3bb
Kseniia <97663572+Kseniiashk@users.noreply.github.com>
2025-06-26 07:01:50 +0900

    Update build.sh

--- resume/build.sh
+++ resume/build.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-mkdir -p /usr/share/texmf/tex/latex/altacv
-cp altacv.cls /usr/share/texmf/tex/latex/altacv/
-texhash
-
-fc-cache -fv
-
+cd CV
 lualatex -interaction=nonstopmode resume.tex
 lualatex -interaction=nonstopmode resume.tex
-
-if [ -f resume.pdf ]; then
-    cp resume.pdf /app/output/
-    echo "PDF успешно сгенерирован!"
-else
-    echo "ОШИБКА: PDF не сгенерирован"
-    ls -la
-    exit 1
-fi
